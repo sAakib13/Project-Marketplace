03fe1274280e0138c64d77c5d404a6e6acad256e
sAakib13 <aakib@telerivet.com>
2025-03-10 11:33:45 +0545

    fix: 3 cols in desktop

--- app/page.tsx
+++ app/page.tsx
@@ -136,7 +136,7 @@ export default function Home() {
         </div>
 
         {/* Project Cards Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {filteredProjects.map((project, projectIndex) => (
             <Card
               key={projectIndex}
