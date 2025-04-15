2e75f113e74e8558ba6b0aabc9f993dd6763d29a
sAakib13 <aakib@telerivet.com>
2025-04-15 13:44:31 +0545

    fix: speed insights added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Oxanium } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const oxanium = Oxanium({
   subsets: ["latin"],
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={oxanium.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
